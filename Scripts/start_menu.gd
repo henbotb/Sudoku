d243b47e7655868f24fbef52bd989015a4353fbc
henbotb <henbotbauer@gmail.com>
2026-02-24 05:17:05 -0700

    Added UI for selecting puzzle difficulty and database of sudoku puzzles for loading from 4 difficulties

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -1,16 +1,31 @@
 extends Control
 
-@onready var button: Button = $Button
 @onready var color_rect: ColorRect = $ColorRect
 @onready var text_edit: TextEdit = $ColorRect/TextEdit
+@onready var option_button: OptionButton = $ColorRect/OptionButton
+
+var rng = RandomNumberGenerator.new()
 
 func _on_button_generate_button_up() -> void:
-	GameState.puzzle_base_board = text_edit.text
+	if text_edit.text.lstrip(" ").rstrip(" ") != "":
+		GameState.puzzle_base_board = text_edit.text
+	else:
+		match option_button.selected:
+			1:
+				get_puzzle_from_difficulty("easy", 100000)
+			3:
+				get_puzzle_from_difficulty("hard", 321592)
+			4:
+				get_puzzle_from_difficulty("diabolical", 119681)
+			2, _:
+				get_puzzle_from_difficulty("medium", 352643)
+				
+		
+				
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 
 func _on_button_copy_button_up() -> void:
-	GameState.puzzle_base_board = DisplayServer.clipboard_get()
-	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+	text_edit.text = DisplayServer.clipboard_get()
 
 func _on_button_start_button_up() -> void:
 	color_rect.visible = true
@@ -22,3 +37,18 @@ func _on_button_quit_button_up() -> void:
 func _input(event):
 	if event.is_action(&"settings"):
 		color_rect.visible = false
+		
+func get_puzzle_from_difficulty(difficulty: String, lines: int):
+	var file = FileAccess.open("res://Assets/Puzzles/%s.txt" % difficulty, FileAccess.READ)
+	var puzzle_num = rng.randi_range(0, lines) - 1
+	var current_line: int = 1
+	
+	while not file.eof_reached() and puzzle_num != current_line:
+		file.get_line()
+		
+	var puzzle_code = file.get_line().substr(13, 81)
+	print("Puzzle code:", puzzle_code)
+	#GameState.puzzle_base_board = file.get_line().substr(13, 81)
+	#print(GameState.puzzle_base_board)
+	
+		
